e47c62132c696da7cef61450fda1f3cc5985fe08
Musaif11 <musaif.pathan13@gmail.com>
2026-09-01 19:18:34 +0530

    Update Java installation to version 21

--- jenkins.sh
+++ jenkins.sh
@@ -6,7 +6,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 #STEP-3: Download Java 17 and Jenkins
-sudo yum install java-17-amazon-corretto -y
+sudo yum install java-21-amazon-corretto -y
 yum install jenkins -y
 
 #STEP-4: Start and check the JENKINS Status

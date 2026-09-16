919d998443e839c56b9f4f8dcc25797120d91474
Musaif11 <musaif.pathan13@gmail.com>
2026-09-16 19:00:00 +0530

    Update SonarQube installation script for Java version

--- sonarqube.sh
+++ sonarqube.sh
@@ -2,7 +2,7 @@
 cd /opt/
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-26.9.0.129388.zip
 unzip sonarqube-8.9.6.50800.zip
-amazon-linux-extras install java-openjdk11 -y
+yum install java-21-amazon-corretto -y
 useradd sonar
 chown sonar:sonar sonarqube-8.9.6.50800 -R
 chmod 777 sonarqube-8.9.6.50800 -R

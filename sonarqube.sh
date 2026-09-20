f2b6f4d6de0fd52b8a602de2eaadf2d068f30ba2
Musaif11 <musaif.pathan13@gmail.com>
2026-09-20 15:50:52 +0530

    Update SonarQube version in installation script

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 cd /opt/
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-26.9.0.129388.zip
-unzip sonarqube-8.9.6.50800.zip
+unzip sonarqube-26.9.0.129388.zip
 yum install java-21-amazon-corretto -y
 useradd sonar
-chown sonar:sonar sonarqube-8.9.6.50800 -R
-chmod 777 sonarqube-8.9.6.50800 -R
+chown sonar:sonar sonarqube-26.9.0.129388 -R
+chmod 777 sonarqube-26.9.0.129388 -R
 su - sonar
 # use the below command manually after installation
 #sh /opt/sonarqube-8.9.6.50800/bin/linux-x86-64/sonar.sh start

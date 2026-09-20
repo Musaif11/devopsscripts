1664ef65ff3440491ddb831c1026e56ef4af0449
Musaif11 <musaif.pathan13@gmail.com>
2026-09-20 14:41:25 +0530

    Update Tomcat version in startup script

--- tomcat.sh
+++ tomcat.sh
@@ -8,4 +8,4 @@ sed -i '59  a\</tomcat-users>' apache-tomcat-10.1.60/conf/tomcat-users.xml
 sed -i '56d' apache-tomcat-10.1.60/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-10.1.60/webapps/manager/META-INF/context.xml
 sed -i '22d' apache-tomcat-10.1.60/webapps/manager/META-INF/context.xml
-sh apache-tomcat-10.1.39/bin/startup.sh
+sh apache-tomcat-10.1.60/bin/startup.sh
